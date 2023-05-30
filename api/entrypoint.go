9925f60ad13c67045d5cece19f9b207b620bc1f4
nitheeshcpu <nitishkoda@outlook.com>
2023-05-30 15:37:12 -0500

    JKHFAGKSDHJF

--- api/entrypoint.go
+++ api/entrypoint.go
@@ -50,6 +50,8 @@ func init() {
 	r := app.Group("/api")
 	myRoute(r)
 
+	app.Static("/static", "./static")
+
 	message := orderedmap.New()
 	message.Set("title", "Welcome to the Home Access Center API!")
 	message.Set("message", "This is the home page, visit the documentation at https://homeaccesscenterapi-docs.vercel.app/ for more information on how to use this API.")
